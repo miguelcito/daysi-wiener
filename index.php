bb3a8f9f36e17094a157bb758565f84b3304ce96
miguel <espinoza@12>
2016-02-29 20:38:17 -0500

    galeano cespedes

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>HOLA PROFESOR  </title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<h1>DEYSI CORAZON</h1>
+<<img src="deysi.gif" alt="">
+<marquee>
+<h1>DEYSI GALEANO CESPEDES</h1>
+<marquee>
+	
+	
+</body>
+</html>
